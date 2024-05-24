4e3a087d14758737717dfc8ce2f2b169eb17fb61
Hydroxycarbamide <nguyen.hoang.eric@protonmail.com>
2024-05-24 14:48:46 +0200

    Add neovide related config

--- init.lua
+++ init.lua
@@ -17,5 +17,14 @@ end
 
 vim.keymap.set("i", "jk", "<esc>")
 
+-- neovide
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  vim.g.neovide_padding_top = 20
+  vim.g.neovide_padding_bottom = 20
+  vim.g.neovide_padding_right = 20
+  vim.g.neovide_padding_left = 20
+end
+
 require "lazy_setup"
 require "polish"
